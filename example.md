6e1c6d86936fb392f74fa69853778a502cdf7bd5
Ryan Hirte <rhirte@ewu.edu>
2024-11-23 16:05:46 -0800

    Handles headings and paragraphs

---
+++ example.md
@@ -0,0 +1,11 @@
+# Heading 1
+
+## Heading 2
+
+### Heading 3
+
+paragraph
+
+- listItem 1
+- listItem 2
+- listItem 3
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 
 // Function to set html boilerplate
@@ -14,6 +16,39 @@ void set_html_boillerplate(FILE *html_file) {
   fprintf(html_file, "<body>\n");
 }
 
+// Function to remove newline character
+void remove_newline(char *buffer) {
+  for (int i = 0; buffer[i] != '\0'; i++) {
+    if (buffer[i] == '\n') {
+      buffer[i] = '\0';
+      break;
+    }
+  }
+}
+
+// Function to mimic strip() 
+void strip_white_space(char *str) {
+  char *end;
+
+  // Trim leading space
+  while (isspace((unsigned char)*str)) {
+    str++;
+  }
+
+  // Return if the line is completely empty
+  if (*str == 0) {
+    return;
+  }
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while (end > str && isspace((unsigned char)*end)) {
+    end--;
+  }
+
+  // Write new null terminator
+  *(end + 1) = 0;
+}
 
 int main(int argc, char *argv[]) {
   // Error handling for incorrect input
@@ -27,8 +62,8 @@ int main(int argc, char *argv[]) {
   char *html_file_name = argv[2];
 
   // Read file
-  FILE *file = fopen(md_file_name, "r");
-  if (file == NULL) {
+  FILE *md_file = fopen(md_file_name, "r");
+  if (md_file == NULL) {
     perror("Unable to open markdown file");
     return EXIT_FAILURE;
   }
@@ -43,12 +78,45 @@ int main(int argc, char *argv[]) {
   // add boilerplate initially
   set_html_boillerplate(html_file);
 
+  // Read the md line by line and determine syntax for html
+  char buffer[1024];
+  while (fgets(buffer, sizeof(buffer), md_file)) {
+
+    // Remove newline character
+    remove_newline(buffer);
+
+    // Strip leading whitespace
+    strip_white_space(buffer);
+
+    // Skip empty lines
+    if (strlen(buffer) == 0) {
+      continue;
+    }
+    // Check for h3, '###' in markdown
+    if (buffer[0] == '#' && buffer[1] == '#' && buffer[2] == '#') {
+      fprintf(html_file, "\t<h3>%s<h3>\n", buffer + 4);
+    }
+    // Check for h2, '##' in markdown
+    else if (buffer[0] == '#' && buffer[1] == '#') {
+      fprintf(html_file, "\t<h2>%s<h2>\n", buffer + 3);
+    }
+    // Check for h1, '#' in markdown
+    else if (buffer[0] == '#') {
+      fprintf(html_file, "\t<h1>%s<h1>\n", buffer + 2);
+    }
+    // else, its a <p>
+    else {
+      fprintf(html_file, "\t<p>%s<p>\n", buffer);
+    }
+    
+  }
+
   // add closing tags
   fprintf(html_file, "</body>\n");
   fprintf(html_file, "</html>\n");
 
   // Close files
-  fclose(file);
+  fclose(md_file);
   fclose(html_file);
   return EXIT_SUCCESS;
 }
